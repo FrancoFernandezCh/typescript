e4ce6e9353d7cfafd6523d59ee5b13dd7f02ca69
Franco Fernández <FrancoFernandezCh@users.noreply.github.com>
2020-11-20 15:32:25 -0300

    changes: propiedades opcionales en una interfaz, ademas se implemento el tipado readonly en una interfaz.
    Signed-off-by: Franco Fernández <FrancoFernandezCh@users.noreply.github.com>

--- dist/12.- interfaces.js
+++ dist/12.- interfaces.js
@@ -1,4 +1,6 @@
 "use strict";
+// INTERFACES
+/* Las interfaces una forma poderosa de definir 'contratos' tanto para tu proyecto, como para el codigo externo */
 Object.defineProperty(exports, "__esModule", { value: true });
 // funcion para mostrar una fotografia
 var PhotoOrientation;
@@ -14,12 +16,32 @@ function showPicture(picture) {
 var myPic = {
     title: "test title",
     date: "2020-03",
-    orientation: PhotoOrientation.Landscape
+    orientation: PhotoOrientation.Landscape,
 };
 showPicture(myPic);
 showPicture({
     title: "test title",
     date: "2020-03",
-    orientation: PhotoOrientation.Portrait
-    //extra: "test" // error
+    orientation: PhotoOrientation.Portrait,
 });
+function generatePicture(config) {
+    var pic = { title: "default", date: "2020-03" };
+    if (config.title) {
+        pic.title = config.title;
+    }
+    if (config.date) {
+        pic.date = config.date;
+    }
+    return pic;
+}
+var picture = generatePicture({});
+console.log("picture", picture);
+picture = generatePicture({ title: "Viaje a Valparaiso" });
+console.log("picture", picture);
+picture = generatePicture({ title: "Viaje a Valparaiso", date: "2018-01" });
+console.log("picture", picture);
+var user;
+user = { id: 12, username: "flashparadox", isPro: true };
+console.log("user", user);
+user.username = "reverseflash";
+console.log("user", user);
--- src/12.- interfaces.ts
+++ src/12.- interfaces.ts
@@ -1,36 +1,82 @@
 // INTERFACES
 /* Las interfaces una forma poderosa de definir 'contratos' tanto para tu proyecto, como para el codigo externo */
 
-export {}
+export {};
 // funcion para mostrar una fotografia
-enum PhotoOrientation{
-    Landscape,
-    Portrait,
-    Square, 
-    Panorama
+enum PhotoOrientation {
+  Landscape,
+  Portrait,
+  Square,
+  Panorama,
 }
 
 interface Picture {
-    title: string,
-    date: string, 
-    orientation: PhotoOrientation
+  title: string;
+  date: string;
+  orientation: PhotoOrientation;
 }
 
 function showPicture(picture: Picture) {
-    console.log(`[title: ${picture.title}, date: ${picture.date}, orientation: ${picture.orientation}]`)
+  console.log(
+    `[title: ${picture.title}, date: ${picture.date}, orientation: ${picture.orientation}]`
+  );
 }
 
 let myPic = {
-    title: "test title",
-    date: "2020-03",
-    orientation: PhotoOrientation.Landscape
+  title: "test title",
+  date: "2020-03",
+  orientation: PhotoOrientation.Landscape,
+};
+
+showPicture(myPic);
+showPicture({
+  title: "test title",
+  date: "2020-03",
+  orientation: PhotoOrientation.Portrait,
+  //extra: "test" // error
+});
+
+interface PictureConfig {
+  title?: string;
+  date?: string;
+  orientation?: PhotoOrientation;
 }
 
-showPicture(myPic)
-showPicture({
-    title: "test title",
-    date: "2020-03",
-    orientation: PhotoOrientation.Portrait
-    //extra: "test" // error
-})
+function generatePicture(config: PictureConfig): Object {
+  const pic = { title: "default", date: "2020-03" };
 
+  if (config.title) {
+    pic.title = config.title;
+  }
+
+  if (config.date) {
+    pic.date = config.date;
+  }
+
+  return pic;
+}
+
+let picture = generatePicture({});
+console.log("picture", picture)
+
+picture = generatePicture({title:"Viaje a Valparaiso"});
+console.log("picture", picture)
+
+picture = generatePicture({title:"Viaje a Valparaiso", date:"2018-01"});
+console.log("picture", picture)
+
+//interfaz: usuario
+interface User{
+    readonly id: number; //solo lectura
+    username: string,
+    isPro: boolean
+}
+
+let user: User;
+user = {id: 12, username:"flashparadox", isPro: true}
+console.log("user", user)
+user.username = "reverseflash"
+console.log("user", user)
+
+//user.id = 20; //error por que es solo readonly
+console.log("user", user)
